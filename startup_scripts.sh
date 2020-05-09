b90d5dd80867a24a46353d714088d601880375ed
Mack <53470440+mayankrajjaiswal@users.noreply.github.com>
2020-05-09 19:43:19 +0530

    Update startup_scripts.sh

--- startup_scripts.sh
+++ startup_scripts.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-python /home/smb/smbserver.py &  
-<<<<<<< HEAD
-<<<<<<< HEAD
-tcpdump -p -f "port 445 or port 139" -w /home/smb/logs/smblog.pcap
-=======
-tcpdump -p -f "port 445 or port 139" -w /home/smb/logs/"smblog.pcap"
->>>>>>> HoneySMB/master
-=======
-tcpdump -p -f "port 445 or port 139" -w /home/smb/logs/smblog.pcap
->>>>>>> HoneySMB/master
+python /home/smb/smbserver.py & tcpdump -p -f "port 445 or port 139" -w /home/smb/logs/smblog.pcap
